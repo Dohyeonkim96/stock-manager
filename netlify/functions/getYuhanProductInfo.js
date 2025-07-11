879871e7b07be2384818930610d0971593008907
Dohyeonkim96 <73j2h@naver.com>
2025-07-11 10:09:53 +0900

    Update getYuhanProductInfo.js

--- netlify/functions/getYuhanProductInfo.js
+++ netlify/functions/getYuhanProductInfo.js
@@ -9,7 +9,6 @@ exports.handler = async function(event, context) {
 
   try {
     const records = await base('기본정보').select({
-      // Airtable에서는 필드 이름에 공백이 있으면 중괄호로 감싸야 합니다.
       filterByFormula: `{유한 품번} = '${yuhanPartNo}'`,
       maxRecords: 1
     }).firstPage();
@@ -22,6 +21,7 @@ exports.handler = async function(event, context) {
       '지에스켐품번': records[0].fields['지에스켐 품번'],
       '품명': records[0].fields['품명'],
       '사업부': records[0].fields['사업부'],
+      '카테고리': records[0].fields['분류'], // '분류' 필드를 '카테고리'로 매핑
     };
 
     return { statusCode: 200, body: JSON.stringify(productInfo) };
