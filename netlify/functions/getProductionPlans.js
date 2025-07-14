c02a4ef442ee6fec69d31126b5ffefe6cb07fc37
Dohyeonkim96 <73j2h@naver.com>
2025-07-14 16:03:37 +0900

    Update getProductionPlans.js

--- netlify/functions/getProductionPlans.js
+++ netlify/functions/getProductionPlans.js
@@ -1,7 +1,8 @@
 exports.handler = async (event) => {
     const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env;
     const TABLE_NAME = '생산계획';
-    const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(TABLE_NAME)}`;
+    const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(TABLE_NAME)}?sort%5B0%5D%5Bfield%5D=생산예정일&sort%5B0%5D%5Bdirection%5D=desc`;
+
     try {
         const response = await fetch(url, {
             headers: { 'Authorization': `Bearer ${AIRTABLE_API_KEY}` }
@@ -10,7 +11,7 @@ exports.handler = async (event) => {
         const data = await response.json();
         return {
             statusCode: 200,
-            body: JSON.stringify(data.records.map(r => ({ id: r.id, fields: r.fields })))
+            body: JSON.stringify(data.records)
         };
     } catch (error) {
         return { statusCode: 500, body: JSON.stringify({ message: error.message }) };
