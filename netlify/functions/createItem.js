0cbe053baa373f8a41df8e57a1cd4f4d4ba42390
Dohyeonkim96 <73j2h@naver.com>
2025-07-14 14:31:10 +0900

    Update createItem.js

--- netlify/functions/createItem.js
+++ netlify/functions/createItem.js
@@ -1,26 +1,14 @@
 const Airtable = require('airtable');
-
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
-const table = base('기본정보'); // '기본정보' 테이블 사용
 
-exports.handler = async (event, context) => {
-    if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
-    }
-
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') return { statusCode: 405, body: 'Method Not Allowed' };
     try {
+        const table = base('기본정보');
         const fields = JSON.parse(event.body);
-        
         const createdRecord = await table.create([{ fields }]);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(createdRecord),
-        };
+        return { statusCode: 200, body: JSON.stringify(createdRecord) };
     } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Error creating record in Airtable: ' + error.message }),
-        };
+        return { statusCode: 500, body: JSON.stringify({ message: '생성 실패: ' + error.message }) };
     }
 };
