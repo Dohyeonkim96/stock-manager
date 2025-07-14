6102a6945590231a3fa5458f554b985f86786bc1
Dohyeonkim96 <73j2h@naver.com>
2025-07-14 14:51:27 +0900

    Update getInventoryData.js

--- netlify/functions/getInventoryData.js
+++ netlify/functions/getInventoryData.js
@@ -3,11 +3,9 @@ exports.handler = async (event) => {
     const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
     const TABLE_NAME = '기본정보';
     
-    // URL에 정렬 옵션 추가 (Airtable API 형식)
     let url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(TABLE_NAME)}?sort%5B0%5D%5Bfield%5D=품목코드&sort%5B0%5D%5Bdirection%5D=asc`;
 
-    // 검색 쿼리가 있는 경우 필터 추가
-    if (event.queryStringParameters.query) {
+    if (event.queryStringParameters && event.queryStringParameters.query) {
         const query = event.queryStringParameters.query;
         const filterFormula = `OR(SEARCH("${query}", {품목코드}), SEARCH("${query}", {품명}))`;
         url += `&filterByFormula=${encodeURIComponent(filterFormula)}`;
